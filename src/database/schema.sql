bdc7fa69d15ec8cb97ae7ca8119cfd7beaa2d81e
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-09-26 13:42:31 -0300

    create publications table in database

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -13,3 +13,12 @@ CREATE TABLE IF NOT EXISTS topics (
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
     name VARCHAR NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS publications (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    sender UUID,
+    text VARCHAR,
+    topic UUID,
+    FOREIGN KEY(sender) REFERENCES users(id),
+    FOREIGN KEY(topic) REFERENCES topics(id)
+);
