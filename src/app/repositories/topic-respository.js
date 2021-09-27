8825cd7a8f98a7463c6f583063be36657d868b23
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-09-27 09:21:04 -0300

    create findAll method in topic-respository

--- src/app/repositories/topic-respository.js
+++ src/app/repositories/topic-respository.js
@@ -1,6 +1,11 @@
 const db = require('../../database');
 
 class TopicRespository {
+  async findAll() {
+    const rows = await db.query('SELECT * FROM topics');
+    return rows;
+  };
+
   async create({ name }) {
     const [row] = await db.query(`
         INSERT INTO topics(name)
